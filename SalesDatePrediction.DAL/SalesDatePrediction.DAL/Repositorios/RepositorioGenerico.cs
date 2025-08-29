4b2b5b31383468562051a69923c503bda3a6f49a
deyved <deyvidfranco41@gmail.com>
2025-08-29 18:05:17 -0500

    creacion capa de negocio y servicios

---
+++ SalesDatePrediction.DAL/SalesDatePrediction.DAL/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,46 @@
+﻿using SalesDatePrediction.DAL.Repositorios.Contratos;
+using SalesDatePrediction.Model.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDatePrediction.DAL.Repositorios
+{
+    public class RepositorioGenerico<TModel> : IGenericRepository<TModel> where TModel : class
+    {
+        private readonly StoreSampleContext _dbFechaVentasContext;
+
+        public RepositorioGenerico(StoreSampleContext dbFechaVentasContext)
+        {
+            _dbFechaVentasContext = dbFechaVentasContext;
+        }
+
+        public Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModel> Crear(TModel modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TModel modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TModel modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
